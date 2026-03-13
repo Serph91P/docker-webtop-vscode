ab7b804b9d6aa68d289eeadf2e32d02d2208b45a
/ <github@mebert-server.de>
2026-03-13 11:54:21 +0100

    Add startup script creation and Nvidia GPU support in startwm.sh

--- root/defaults/startwm.sh
+++ root/defaults/startwm.sh
@@ -28,3 +28,30 @@ if [ ! -d $HOME/.config/kde.org ]; then
     done
   ) &
 fi
+
+# Create startup script if it does not exist (keep in sync with openbox)
+STARTUP_FILE="${HOME}/.config/autostart/autostart.desktop"
+if [ ! -f "${STARTUP_FILE}" ]; then
+  echo "[Desktop Entry]" > $STARTUP_FILE
+  echo "Exec=bash /config/.config/openbox/autostart" >> $STARTUP_FILE
+  echo "Icon=dialog-scripts" >> $STARTUP_FILE
+  echo "Name=autostart" >> $STARTUP_FILE
+  echo "Path=" >> $STARTUP_FILE
+  echo "Type=Application" >> $STARTUP_FILE
+  echo "X-KDE-AutostartScript=true" >> $STARTUP_FILE
+  chmod +x $STARTUP_FILE
+fi
+
+# Enable Nvidia GPU support if detected
+if which nvidia-smi > /dev/null 2>&1 && ls -A /dev/dri 2>/dev/null && [ "${DISABLE_ZINK}" == "false" ]; then
+  export LIBGL_KOPPER_DRI2=1
+  export MESA_LOADER_DRIVER_OVERRIDE=zink
+  export GALLIUM_DRIVER=zink
+fi
+
+# Dbus defaults
+export XDG_RUNTIME_DIR="/tmp/xdg-runtime-abc"
+mkdir -p -m700 "${XDG_RUNTIME_DIR}"
+
+# Start DE
+exec dbus-launch /usr/bin/startplasma-x11 > /dev/null 2>&1
